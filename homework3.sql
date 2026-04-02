ad84b53424a194624261ad655b2e8b837fe54a23
ssswitzzz <1290757258@qq.com>
2026-04-02 10:46:14 +0800

    更正homework3答案

--- homework3.sql
+++ homework3.sql
@@ -20,11 +20,11 @@ join categories c on c.categoryid = p.categoryid
 where c.categoryname = 'Beverages';
 
 select p1.productname, count(p2.productid) as similar_price_count from products p1
-join products p2 on p1.categoryid = p2.categoryid and p1.productid <> p2.productid
-where abs(p1.unitprice - p2.unitprice) <=10 group by p1.productname order by similar_price_count desc;
+left join products p2 on p1.categoryid = p2.categoryid and p1.productid <> p2.productid and abs(p1.unitprice - p2.unitprice) <=10
+group by p1.productname order by similar_price_count desc;
 
 select s.country, count(distinct p.categoryid) from suppliers s join products p on s.supplierid = p.supplierid
-group by s.country order by count(p.categoryid) desc limit 1;
+group by s.country order by count(distinct p.categoryid) desc limit 1;
 
 select c.categoryname, count(distinct p.supplierid) as supplier_count from categories c
 join products p on c.categoryid = p.categoryid
@@ -38,7 +38,7 @@ select e1.firstname || ' ' || e1.lastname as employee from employees e1
 join employees e2 on e1.reportsto = e2.employeeid
 where e2.firstname = 'Andrew' and e2.lastname = 'Fuller';
 
-select current_date - hiredate as hire_days, firstname, extract(year from age(current_date, hiredate)) as age from employees
+select current_date - hiredate as hire_days, firstname, extract(year from age(current_date, birthdate)) as age from employees
 order by hire_days desc;
 
 select e.firstname, e.lastname, round((count(o.orderid)::numeric/(current_date - e.hiredate)),4) as order_per_day from employees e
@@ -46,7 +46,7 @@ join orders o on e.employeeid = o.employeeid
 group by e.employeeid, e.hiredate, e.firstname, e.lastname order by order_per_day desc;
 
 select e1.firstname, count(e2.employeeid) as younger_employees from employees e1
-join employees e2 on e2.birthdate > e1.birthdate
+left join employees e2 on e2.birthdate > e1.birthdate
 group by e1.firstname order by younger_employees desc;
 
 select distinct s.country, s.city from suppliers s
